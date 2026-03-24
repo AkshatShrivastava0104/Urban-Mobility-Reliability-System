71446550863c92dbf40f8ccfb9611b8ab038795c
Akshat <121187184+AkshatShrivastava0104@users.noreply.github.com>
2026-03-25 00:11:44 +0530

    first commit

---
+++ main.go
@@ -0,0 +1,65 @@
+// package main
+
+// import "fmt"
+
+// type Car struct{
+// 	ID int
+// 	Name string
+// 	Price float32
+// }
+
+
+// // var c Car
+
+// // var c Car = Car{
+// // 	ID: 1,
+// // 	Name: "BMW",
+// // 	Price: 30000.50,
+// // }
+
+
+
+// // fmt.Println(d.ID)
+
+// func updatePrice(c *Car){
+// 	c.Price = 35000.75
+// }
+
+// func main(){
+// 	fmt.Println("Hello Worlds")
+
+// 	x := 100
+// 	{
+// 		x := 200
+// 		fmt.Println(x)
+// 	}
+// 	fmt.Println(x)
+
+
+
+// 	d := Car{
+// 		ID: 1,
+// 		Name: "BMW",
+// 		Price: 30000.50,
+// 	}
+
+
+// 	fmt.Println(d.ID)
+
+// 	d.Name = "Audi"
+
+
+// 	fmt.Println(d.Name)
+
+// 	updatePrice(&d)
+
+// 	fmt.Println(d.Price)
+
+
+// 	// var y string = "100"
+// 	// fmt.Println(y)
+// }
+
+
+
+
